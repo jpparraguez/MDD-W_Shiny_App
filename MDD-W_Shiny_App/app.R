96cb8f6d6feadc81b17c7f14dfbc584ee83b7814
Juan Pablo Parraguez Montecinos <jpparraguez@uc.cl>
2023-09-18 10:01:37 -0300

    Import data ready. Working on download templates

--- MDD-W_Shiny_App/app.R
+++ MDD-W_Shiny_App/app.R
@@ -13,43 +13,47 @@ ui <- fluidPage(
   sidebarLayout(
     # Sidebar panel for inputs
     sidebarPanel(
-      ##### Download material ----
-      # Title
-      tags$h3("Downloadable material"),
-      # Input_download: Choose dataset
-      selectInput("dataset",
-                  "Choose a dataset:",
-                  choices = c("MDD-W Food Groups", "MDD-W upload data template")),
-      # Button_download
-      downloadButton("downloadData", "Download"),
-      
-      # Horizontal line
-      tags$hr(),
-      
       ##### Upload material ----
       # Title
       tags$h3("Upload your data"),
       # Input_upload: Select a file
-      fileInput("file1", "Choose CSV File",
+      fileInput("loaded_file", "Choose CSV File",
                 multiple = FALSE,
                 accept = c("text/csv",
                            "text/comma-separated-values,text/plain",
                            ".csv")
-                ),
+      ),
+      # Horizontal line
+      tags$hr(),
+      
+      
+      ##### Download material ----
+      # Title
+      tags$h3("Downloadable material"),
+      # Input_download: Choose dataset
+      radioButtons("select_template", "Templates",
+                   choices = c("MDD-W Food Groups",
+                               "MDD-W upload data template")
+                   ),
+
+      # Button_download
+      downloadButton("downloadData", "Download"),
       
     ),
-    ### Main panel for displaying outputs ----
+    ## Main panel for displaying outputs ----
     mainPanel(
-      # Output: Tabset w/ plot, summary, and table ----
+      # Output:
       tabsetPanel(type = "tabs",
-                  tabPanel("Templates",
-                           plotOutput("Template")),
+                  tabPanel("Preview Imported Data",
+                           tableOutput("preview_loaded_file")),
                   tabPanel("MDD-W Prevalence",
                            plotOutput("plot")),
                   tabPanel("FG Distribution",
                            verbatimTextOutput("summary")),
                   tabPanel("Unhealthy Food Groups",
-                           tableOutput("table"))
+                           tableOutput("table")),
+                  tabPanel("Templates",
+                           tableOutput("select_template"))
                   )
       )
     )
@@ -58,8 +62,28 @@ ui <- fluidPage(
 
 # Server ----
 server <- function(input, output) {
+  ## Render Preview_import ----
+  output$preview_loaded_file <- renderTable({
+    # Check for required values before proceeding with next step. If not true, stop operation.
+    req(input$loaded_file)
+    
+    # Store readed file in an object
+    imported_data <- read.csv(input$loaded_file$datapath)
+    # Return the object
+    return(imported_data)
+    
+    })
   
-
+  ## Download templates ----
+  output$select_template <- downloadHandler(
+    filename = function() {
+      paste(input$dataset, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(datasetInput(), file, row.names = FALSE)
+    }
+  )
 }
+
 # Run the app ----
 shinyApp(ui, server)
