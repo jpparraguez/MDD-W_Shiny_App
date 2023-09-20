bf96c5f84a68e8eb1d3e829a6182e566b386f626
Juan Pablo Parraguez Montecinos <jpparraguez@uc.cl>
2023-09-20 14:52:44 -0300

    Managed to add a download button to get the MDD-W template.

--- MDD-W_Shiny_App/app.R
+++ MDD-W_Shiny_App/app.R
@@ -4,48 +4,47 @@
 # Libraries
 library(shiny)
 library(tidyverse)
+library(data.table)
+library(readxl)
 
 # UI ----
-ui <- fluidPage(
-  ## App title ----
-  titlePanel("MDD-W Shiny App"),
-  ### Sidebar layout with input and output definitions ----
+ui <- fluidPage(titlePanel("MDD-W Shiny App"),
+  ### Sidebar  ----
   sidebarLayout(
-    # Sidebar panel for inputs
+    #### Load material ----
     sidebarPanel(
-      ##### Upload material ----
       # Title
-      tags$h3("Upload your data"),
+      tags$h3("Upload your data from Excel (.xlsx)"),
       # Input_upload: Select a file
       fileInput("loaded_file", "Choose CSV File",
                 multiple = FALSE,
-                accept = c("text/csv",
-                           "text/comma-separated-values,text/plain",
-                           ".csv")
+                accept = c(".xlsx")
       ),
       # Horizontal line
       tags$hr(),
       
       
-      ##### Download material ----
+      #### Download material ----
       # Title
       tags$h3("Downloadable material"),
-      # Input_download: Choose dataset
-      radioButtons("select_template", "Templates",
-                   choices = c("MDD-W Food Groups",
-                               "MDD-W upload data template")
-                   ),
-
+      
       # Button_download
-      downloadButton("downloadData", "Download"),
+      downloadButton("download_template", "Download Template"),
       
     ),
-    ## Main panel for displaying outputs ----
+    ### Main panel ----
     mainPanel(
       # Output:
       tabsetPanel(type = "tabs",
+                  #### Tab1: Preview import ----
                   tabPanel("Preview Imported Data",
                            tableOutput("preview_loaded_file")),
+                  #### Tab2: Describe imported data ----
+                  tabPanel("Data description",
+                           tags$h4("Number of initial observations:"),
+                           textOutput("nrow_holder"),
+                           tags$h4("Number columns:"),
+                           textOutput("ncol_holder")),
                   tabPanel("MDD-W Prevalence",
                            plotOutput("plot")),
                   tabPanel("FG Distribution",
@@ -61,29 +60,53 @@ ui <- fluidPage(
       
 
 # Server ----
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
+  # Save loaded data as reactive value. This way it can be used across.
+  imported_data <- reactive({
+    # Check for required values before proceeding with next step. If not true, stop operation.
+    req(input$loaded_file)    
+    # Read the imported value. It's stored as "imported_data".
+    # read.csv(input$loaded_file$datapath)
+    read_excel(input$loaded_file$datapath)
+    })
+  
   ## Render Preview_import ----
   output$preview_loaded_file <- renderTable({
-    # Check for required values before proceeding with next step. If not true, stop operation.
-    req(input$loaded_file)
-    
-    # Store readed file in an object
-    imported_data <- read.csv(input$loaded_file$datapath)
-    # Return the object
-    return(imported_data)
+    # # Check for required values before proceeding with next step. If not true, stop operation.
+    # req(input$loaded_file)
+    # # Store readed file in an object
+    # imported_data <- read.csv(input$loaded_file$datapath)
+    # # Return the object
+    data.table(imported_data())
     
     })
   
-  ## Download templates ----
-  output$select_template <- downloadHandler(
+  
+  
+  
+  ## Descriptive values ----
+    output$nrow_holder <- renderText({
+    nrow(imported_data())
+  })
+  output$ncol_holder <- renderText({
+    ncol(imported_data())
+  })
+ 
+  
+  # Download template
+  output$download_template <- downloadHandler(
     filename = function() {
-      paste(input$dataset, ".csv", sep = "")
+      paste("MDD-W Template", ".xlsx", sep='')
     },
     content = function(file) {
-      write.csv(datasetInput(), file, row.names = FALSE)
+      # myfile <- srcpath <- 'Home/Other Layer/Fancy Template.xlsx'
+      myfile <- srcpath <-  "./www/Template_for_upload.xlsx"
+      file.copy(myfile, file)
     }
-  )
+  ) 
+    
 }
-
+  
 # Run the app ----
 shinyApp(ui, server)
